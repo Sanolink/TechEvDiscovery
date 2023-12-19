97c341bf98a48a9b153a8f6b8a5ba0b00b15cfee
SanoLink <link4francais@gmail.com>
2023-12-19 03:09:47 +0100

    (Blue / Source) Quartz (Items + Blocks)

---
+++ kubejs/startup_scripts/block.js
@@ -0,0 +1,16 @@
+/* 
+Code By SanoLink For TechEV - Discovery
+Last Modification : 1.1.0 
+*/
+
+Platform.mods.kubejs.name = 'TechEv || Discovery'
+
+StartupEvents.registry('block', e => {
+
+    e.create('create:blue_quartz_block').displayName("Block Of Blue Quartz").soundType("deepslate").property(BlockProperties.AXIS).placementState(event => event.set(BlockProperties.AXIS, event.clickedFace.axis)).hardness(1.5).tagBlock("mineable/pickaxe").requiresTool(true)
+    e.create('create:blue_quartz_tiles').displayName("Blue Quartz Tiles").soundType("deepslate").hardness(3).tagBlock("mineable/pickaxe").requiresTool(true)
+    e.create('create:small_blue_quartz_tiles').displayName("Small Blue Quartz Tiles").soundType("deepslate").hardness(3).tagBlock("mineable/pickaxe").requiresTool(true)
+    e.create('create:source_quartz_block').displayName("Block Of Source Quartz").soundType("deepslate").property(BlockProperties.AXIS).placementState(event => event.set(BlockProperties.AXIS, event.clickedFace.axis)).hardness(1.5).tagBlock("mineable/pickaxe").requiresTool(true)
+    e.create('create:source_quartz_tiles').displayName("Source Quartz Tiles").soundType("deepslate").hardness(3).tagBlock("mineable/pickaxe").requiresTool(true)
+    e.create('create:small_source_quartz_tiles').displayName("Small Source Quartz Tiles").soundType("deepslate").hardness(3).tagBlock("mineable/pickaxe").requiresTool(true)
+})
--- kubejs/startup_scripts/item.js
+++ kubejs/startup_scripts/item.js
@@ -14,6 +14,12 @@ StartupEvents.registry('item', e => {
     e.create('core_iron').displayName("Iron Wand Core")
     e.create('core_diamond').displayName("Diamond Wand Core")
     e.create('core_infinity').displayName("Infinity Wand Core")
+    e.create('create:blue_quartz').displayName("Blue Quartz")
+    e.create('create:polished_blue_quartz').displayName("Polished Blue Quartz")
+    e.create('create:positron_tube').displayName("Positron Tube")
+    e.create('create:source_quartz').displayName("Source Quartz")
+    e.create('create:polished_source_quartz').displayName("Polished Source Quartz")
+    e.create('create:source_tube').displayName("Source Tube")
 
   })
 

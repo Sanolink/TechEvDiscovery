2753218c8a0b8e87651ffac06444e975956cd45e
SanoLink <link4francais@gmail.com>
2023-12-23 03:51:11 +0100

    Added Custom Items

--- kubejs/startup_scripts/item.js
+++ kubejs/startup_scripts/item.js
@@ -1,19 +1,21 @@
-/* 
-Code By SanoLink For TechEV - Discovery
-Last Modification : 1.1.0 
-*/
+//Code By SanoLink For TechEV - Discovery
+//Last Modification : 1.1.0 
+
 
 Platform.mods.kubejs.name = 'TechEv || Discovery'
 
 StartupEvents.registry('item', e => {
 
-    e.create('incomplete_hammer').maxStackSize(1).displayName("Incomplete Engineer's Hammer")
-    e.create('incomplete_wirecutter').maxStackSize(1).displayName("Incomplete Engineer's Wire Cutters")
-    e.create('fishing_rod_cast').maxStackSize(1).displayName("Fishing Rod Cast")
-    e.create('core_stone').displayName("Stone Wand Core")
-    e.create('core_iron').displayName("Iron Wand Core")
-    e.create('core_diamond').displayName("Diamond Wand Core")
-    e.create('core_infinity').displayName("Infinity Wand Core")
+    e.create('minecraft:fishing_rod_cast', 'create:sequenced_assembly').displayName("Fishing Rod Cast")
+
+    e.create('immersiveengineering:incomplete_hammer', 'create:sequenced_assembly').displayName("Incomplete Engineer's Hammer")
+    e.create('immersiveengineering:incomplete_wirecutter', 'create:sequenced_assembly').displayName("Incomplete Engineer's Wire Cutters")
+
+    e.create('constructionwand:core_stone').displayName("Stone Wand Core")
+    e.create('constructionwand:core_iron').displayName("Iron Wand Core")
+    e.create('constructionwand:core_diamond').displayName("Diamond Wand Core")
+    e.create('constructionwand:core_infinity').displayName("Infinity Wand Core")
+
     e.create('create:blue_quartz').displayName("Blue Quartz")
     e.create('create:polished_blue_quartz').displayName("Polished Blue Quartz")
     e.create('create:positron_tube').displayName("Positron Tube")

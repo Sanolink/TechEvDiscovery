5a2766fcf329acb86dffa8c9f5c5262b3a211849
SanoLink <link4francais@gmail.com>
2024-01-20 23:39:41 +0100

    Remove Villager Trades [Electrician / Machinist]

---
+++ kubejs/server_scripts/trade.js
@@ -0,0 +1,9 @@
+//Code By SanoLink For TechEV - Discovery
+//Last Modification : 1.1.2 
+
+MoreJSEvents.villagerTrades(event => {
+
+    event.removeModdedTrades("immersiveengineering:electrician", 1)
+    event.removeModdedTrades("immersiveengineering:machinist", 1)
+    
+})

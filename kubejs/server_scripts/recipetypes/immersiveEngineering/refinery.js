5998c3574059a08677416d42d573ec64da5f6894
SanoLink <link4francais@gmail.com>
2024-01-26 03:05:10 +0100

    remove Acetaldehyde from silver

---
+++ kubejs/server_scripts/recipetypes/immersiveEngineering/refinery.js
@@ -0,0 +1,9 @@
+//Code By SanoLink For TechEV - Discovery
+//Last Modification : 1.1.4
+
+ServerEvents.recipes(event => {
+
+    //Remove
+    event.remove({ id: 'immersiveengineering:refinery/acetaldehyde'})
+
+})

82df3ef388d1938012c807dd7220ad72b1adecd4
SanoLink <link4francais@gmail.com>
2025-03-12 00:55:50 +0100

    [✨] Resonant Machine Frame

--- kubejs/startup_scripts/block.js
+++ kubejs/startup_scripts/block.js
@@ -19,4 +19,6 @@ StartupEvents.registry('block', e => {
     e.create('kubejs:raw_cuperzinate_block').displayName("Block Of Raw Cuperzinate").soundType("tuff").hardness(3).tagBlock("mineable/pickaxe").requiresTool(true)
     e.create('kubejs:raw_leadosnite_block').displayName("Block Of Raw Leadosnite").soundType("tuff").hardness(3).tagBlock("mineable/pickaxe").requiresTool(true)
     e.create('kubejs:raw_uratinumal_block').displayName("Block Of Raw Uratinumal").soundType("tuff").hardness(3).tagBlock("mineable/pickaxe").requiresTool(true)
+
+    e.create('kubejs:resonant_machine_frame').displayName("Resonant Machine Frame").soundType("lantern").hardness(2).tagBlock("mineable/pickaxe")
 })

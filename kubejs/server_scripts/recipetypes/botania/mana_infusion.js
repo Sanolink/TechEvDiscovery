56278351c1fd79a8e774eefd75a0f649d3f48e15
SanoLink <link4francais@gmail.com>
2025-03-16 02:35:51 +0100

    [✨] Botania Craft Fertilizer

---
+++ kubejs/server_scripts/recipetypes/botania/mana_infusion.js
@@ -0,0 +1,38 @@
+/* 
+ _____                                             _____ 
+( ___ ) © SanoLink 2024/2025. All rights reserved.( ___ )
+ |   |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|   | 
+ |   |  _____         _     _____        ______    |   | 
+ |   | |_   _|__  ___| |__ | ____|_   __ \ \ \ \   |   | 
+ |   |   | |/ _ \/ __| '_ \|  _| \ \ / /  \ \ \ \  |   | 
+ |   |   | |  __/ (__| | | | |___ \ V /   / / / /  |   | 
+ |   |  _|_|\___|\___|_| |_|_____| \_/   /_/_/_/   |   | 
+ |   | |  _ \(_)___  ___ _____   _____ _ __ _   _  |   | 
+ |   | | | | | / __|/ __/ _ \ \ / / _ \ '__| | | | |   | 
+ |   | | |_| | \__ \ (_| (_) \ V /  __/ |  | |_| | |   | 
+ |   | |____/|_|___/\___\___/ \_/ \___|_|   \__, | |   | 
+ |   |                                      |___/  |   | 
+ |___|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|___| 
+(_____)         Last Modification : 1.3.0         (_____)
+
+*/
+
+
+ServerEvents.recipes(event => {
+
+    //Recipes
+    const recipes = [
+        {
+            id: 'fertilizer',
+            output: 'botania:fertilizer',
+            inputs: 'minecraft:bone_meal',
+            mana: 2500,
+            catalyst: 'botania:floral_catalyst'
+        }
+    ]
+
+    //General Mana Infusion Function
+    recipes.forEach(recipe => {
+        event.recipes.botania.mana_infusion(recipe.output, recipe.inputs, recipe.mana, recipe.catalyst).id("botania:mana_infusion/" + recipe.id)
+    })
+})

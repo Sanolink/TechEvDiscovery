11a964994a79f0eaabf9d614def046bb4f9385d9
SanoLink <link4francais@gmail.com>
2025-03-22 02:10:14 +0100

    [✨] Synthetic Mana Diamond

--- kubejs/startup_scripts/fluid.js
+++ kubejs/startup_scripts/fluid.js
@@ -1,5 +1,22 @@
-//Code By SanoLink For TechEV - Discovery
-//Last Modification : 1.1.0 
+/* 
+ _____                                             _____ 
+( ___ ) © SanoLink 2024/2025. All rights reserved.( ___ )
+ |   |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|   | 
+ |   |  _____         _     _____        ______    |   | 
+ |   | |_   _|__  ___| |__ | ____|_   __ \ \ \ \   |   | 
+ |   |   | |/ _ \/ __| '_ \|  _| \ \ / /  \ \ \ \  |   | 
+ |   |   | |  __/ (__| | | | |___ \ V /   / / / /  |   | 
+ |   |  _|_|\___|\___|_| |_|_____| \_/   /_/_/_/   |   | 
+ |   | |  _ \(_)___  ___ _____   _____ _ __ _   _  |   | 
+ |   | | | | | / __|/ __/ _ \ \ / / _ \ '__| | | | |   | 
+ |   | | |_| | \__ \ (_| (_) \ V /  __/ |  | |_| | |   | 
+ |   | |____/|_|___/\___\___/ \_/ \___|_|   \__, | |   | 
+ |   |                                      |___/  |   | 
+ |___|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|___| 
+(_____)         Last Modification : 1.3.0         (_____)
+
+*/
+
 
 StartupEvents.registry('fluid', e => {
 
@@ -12,4 +29,8 @@ StartupEvents.registry('fluid', e => {
       .stillTexture('hostilenetworks:fluid/polymer_clay_still')
       .flowingTexture('hostilenetworks:fluid/polymer_clay_flow')
 
+    e.create('botania:synthetic_mana_diamond')
+      .displayName('Synthetic Mana Diamond')
+      .stillTexture('botania:fluid/synthetic_mana_diamond_still')
+      .flowingTexture('botania:fluid/synthetic_mana_diamond_flow')
   })

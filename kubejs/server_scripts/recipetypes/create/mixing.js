1925768f2dda9843ae45c863db9aa15242590df8
SanoLink <link4francais@gmail.com>
2025-03-22 02:10:14 +0100

    [✨] Synthetic Mana Diamond

--- kubejs/server_scripts/recipetypes/create/mixing.js
+++ kubejs/server_scripts/recipetypes/create/mixing.js
@@ -1,5 +1,21 @@
-//Code By SanoLink For TechEV - Discovery
-//Last Modification : 1.1.0
+/* 
+ _____                                             _____ 
+( ___ ) © SanoLink 2024/2025. All rights reserved.( ___ )
+ |   |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|   | 
+ |   |  _____         _     _____        ______    |   | 
+ |   | |_   _|__  ___| |__ | ____|_   __ \ \ \ \   |   | 
+ |   |   | |/ _ \/ __| '_ \|  _| \ \ / /  \ \ \ \  |   | 
+ |   |   | |  __/ (__| | | | |___ \ V /   / / / /  |   | 
+ |   |  _|_|\___|\___|_| |_|_____| \_/   /_/_/_/   |   | 
+ |   | |  _ \(_)___  ___ _____   _____ _ __ _   _  |   | 
+ |   | | | | | / __|/ __/ _ \ \ / / _ \ '__| | | | |   | 
+ |   | | |_| | \__ \ (_| (_) \ V /  __/ |  | |_| | |   | 
+ |   | |____/|_|___/\___\___/ \_/ \___|_|   \__, | |   | 
+ |   |                                      |___/  |   | 
+ |___|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|___| 
+(_____)         Last Modification : 1.3.0         (_____)
+
+*/
 
 ServerEvents.recipes(event => {
 
@@ -28,6 +44,13 @@ ServerEvents.recipes(event => {
             input: ['minecraft:clay_ball', Fluid.of("thermal:resin", 125), Fluid.of("pneumaticcraft:plastic", 125)],
             output: Fluid.of("hostilenetworks:polymer_clay", 250)
         },
+        {
+            id: 'synthetic_mana_diamond',
+            type: 'heated',
+            input: [Fluid.of("create_enchantment_industry:hyper_experience", 50), Fluid.of("pneumaticcraft:yeast_culture", 1000), "botania:infused_seeds" , "9x minecraft:diamond", "3x forbidden_arcanus:arcane_crystal_dust"],
+            output: Fluid.of("botania:synthetic_mana_diamond", 1000),
+            time: 1000
+        },
         //Superheated Recipes
         {
             id: 'molten_redstone',
@@ -51,11 +74,11 @@ ServerEvents.recipes(event => {
     //General Mixing Function
     recipes.forEach(recipe => {
         if (recipe.type == 'basic') {
-            event.recipes.create.mixing(recipe.output, recipe.input).id("create:mixing/" + recipe.id)
+            event.recipes.create.mixing(recipe.output, recipe.input, recipe.time ?? 100).id("create:mixing/" + recipe.id)
         } else if (recipe.type == 'heated') {
-            event.recipes.create.mixing(recipe.output, recipe.input).id("create:mixing/" + recipe.id).heated()
+            event.recipes.create.mixing(recipe.output, recipe.input, recipe.time ?? 100).id("create:mixing/" + recipe.id).heated()
         } else if (recipe.type == 'superheated') {
-            event.recipes.create.mixing(recipe.output, recipe.input).id("create:mixing/" + recipe.id).superheated()
+            event.recipes.create.mixing(recipe.output, recipe.input, recipe.time ?? 100).id("create:mixing/" + recipe.id).superheated()
         }
     })
 })

caf734d691fd88484eedf651c43cf881850b64d1
SanoLink <link4francais@gmail.com>
2025-05-04 02:23:05 +0200

    [✨] HNN Predictions Recipes

--- kubejs/server_scripts/recipetypes/crafting/hostileNetworks.js
+++ kubejs/server_scripts/recipetypes/crafting/hostileNetworks.js
@@ -22,21 +22,63 @@ ServerEvents.recipes(event => {
    //Recipes
    const recipes = [
     
-    //Inert Data Model
-    {
-        output: 'hostilenetworks:inert_data_model',
-        pattern: ["UMU", "AEB", "UMU"],
-        key: {
-            U: 'undergarden:utherium_crystal',
-            M: 'pneumaticcraft:module_expansion_card',
-            A: 'ae2:advanced_card',
-            E: 'powah:ender_core',
-            B: 'ae2:basic_card'
-        }
-    },
-]
+        //Inert Data Model
+        {
+            output: 'hostilenetworks:inert_data_model',
+            pattern: ["UMU", "AEB", "UMU"],
+            key: {
+                U: 'undergarden:utherium_crystal',
+                M: 'pneumaticcraft:module_expansion_card',
+                A: 'ae2:advanced_card',
+                E: 'powah:ender_core',
+                B: 'ae2:basic_card'
+            }
+        },
+    ]
 
-recipes.forEach((recipe) => {
-    event.shaped(recipe.output, recipe.pattern, recipe.key).id(recipe.output)
-})
+    const shapeless = [
+        {
+            id: 'undergarden:depthrock_pebble',
+            output: '64x undergarden:depthrock_pebble',
+            input: ['hostilenetworks:undergarden_prediction', '#forge:cobblestone']
+        },
+        {
+            id: 'undergarden:gloomgourd',
+            output: '4x undergarden:gloomgourd',
+            input: ['hostilenetworks:undergarden_prediction', 'minecraft:pumpkin']
+        },
+        {
+            id: 'undergarden:gloomgourd_pie',
+            output: '4x undergarden:gloomgourd_pie',
+            input: ['hostilenetworks:undergarden_prediction', 'minecraft:pumpkin_pie']
+        },
+        {
+            id: 'blue_skies:cherry',
+            output: '8x blue_skies:cherry',
+            input: ['hostilenetworks:blue_skies_prediction', 'minecraft:sweet_berries']
+        },
+        {
+            id: 'blue_skies:blinding_stone',
+            output: '8x blue_skies:blinding_stone',
+            input: ['hostilenetworks:blue_skies_prediction', '#forge:stone']
+        },
+        {
+            id: 'blue_skies:soul_fragment',
+            output: '16x blue_skies:soul_fragment',
+            input: ['hostilenetworks:blue_skies_prediction', 'forbidden_arcanus:soul']
+        },
+        {
+            id: 'blue_skies:snowcap_pinhead',
+            output: '8x blue_skies:snowcap_pinhead',
+            input: ['hostilenetworks:blue_skies_prediction', '#forge:mushrooms']
+        }
+    ]
+
+    recipes.forEach((recipe) => {
+        event.shaped(recipe.output, recipe.pattern, recipe.key).id(recipe.output)
+    })
+
+    shapeless.forEach((recipe) => {
+        event.shapeless(recipe.output, recipe.input).id(recipe.id)
+    })
 })

db18bca16e5d46adfa9389eaf23f4dd691957391
SanoLink <link4francais@gmail.com>
2025-05-16 00:53:30 +0200

    [✨] Age 5 Unification

--- kubejs/client_scripts/jei.js
+++ kubejs/client_scripts/jei.js
@@ -13,7 +13,7 @@
  |   | |____/|_|___/\___\___/ \_/ \___|_|   \__, | |   | 
  |   |                                      |___/  |   | 
  |___|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|___| 
-(_____)         Last Modification : 1.3.2         (_____)
+(_____)         Last Modification : 1.4.0         (_____)
 
 */
 
@@ -30,58 +30,63 @@ JEIEvents.hideItems(event => {
         
         //Ores
         'create:zinc_ore', 
-        'eidolon:lead_ore', 'immersiveengineering:ore_lead', 'thermal:lead_ore',
+        'eidolon:lead_ore', 'immersiveengineering:ore_lead', 'thermal:lead_ore', 'mekanism:lead_ore',
         'eidolon:silver_ore', 'immersiveengineering:ore_silver', 'darkerdepths:limestone_silver_ore', 'darkerdepths:aridrock_silver_ore', 'darkerdepths:silver_ore', 'iceandfire:silver_ore', 'occultism:silver_ore', 'thermal:silver_ore',
         'iceandfire:sapphire_ore',
         'immersiveengineering:ore_aluminum',
         'immersiveengineering:ore_nickel', 'thermal:nickel_ore',
-        'immersiveengineering:ore_uranium', 
-        'thermal:tin_ore',
+        'immersiveengineering:ore_uranium', 'mekanism:uranium_ore',
+        'mekanism:osmium_ore',
+        'thermal:tin_ore', 'mekanism:tin_ore',
         
         //Deepslate Ores
         'create:deepslate_zinc_ore', 
-        'eidolon:deep_lead_ore', 'immersiveengineering:deepslate_ore_lead', 'thermal:deepslate_lead_ore',
+        'eidolon:deep_lead_ore', 'immersiveengineering:deepslate_ore_lead', 'thermal:deepslate_lead_ore', 'mekanism:deepslate_lead_ore',
         'eidolon:deep_silver_ore', 'immersiveengineering:deepslate_ore_silver', 'iceandfire:deepslate_silver_ore', 'occultism:silver_ore_deepslate', 'thermal:deepslate_silver_ore',
         'immersiveengineering:deepslate_ore_aluminum',
         'immersiveengineering:deepslate_ore_nickel', 'thermal:deepslate_nickel_ore',
-        'immersiveengineering:deepslate_ore_uranium', 
-        'thermal:deepslate_tin_ore',
+        'immersiveengineering:deepslate_ore_uranium', 'mekanism:deepslate_uranium_ore', 
+        'mekanism:deepslate_osmium_ore',
+        'thermal:deepslate_tin_ore', 'mekanism:deepslate_tin_ore',
         
         //Raw Materials
         'create:raw_zinc',
-        'eidolon:raw_lead', 'immersiveengineering:raw_lead', 'thermal:raw_lead',
+        'eidolon:raw_lead', 'immersiveengineering:raw_lead', 'thermal:raw_lead', 'mekanism:raw_lead',
         'eidolon:raw_silver', 'immersiveengineering:raw_silver', 'darkerdepths:raw_silver', 'occultism:raw_silver', 'thermal:raw_silver',
         'immersiveengineering:raw_aluminum', 
         'immersiveengineering:raw_nickel', 'thermal:raw_nickel',
-        'immersiveengineering:raw_uranium', 
-        'thermal:raw_tin',
+        'mekanism:block_raw_osmium',
+        'immersiveengineering:raw_uranium', 'mekanism:raw_uranium', 
+        'mekanism:raw_osmium', 
+        'thermal:raw_tin', 'mekanism:raw_tin',
         
         //Raw Storage Blocks
         'create:raw_zinc_block', 
-        'eidolon:raw_lead_block', 'immersiveengineering:raw_block_lead', 'thermal:raw_lead_block',
+        'eidolon:raw_lead_block', 'immersiveengineering:raw_block_lead', 'thermal:raw_lead_block', 'mekanism:block_raw_lead',
         'eidolon:raw_silver_block', 'immersiveengineering:raw_block_silver', 'darkerdepths:raw_silver_block', 'thermal:raw_silver_block',
         'immersiveengineering:raw_block_aluminum',
         'immersiveengineering:raw_block_nickel', 'thermal:raw_nickel_block', 
-        'immersiveengineering:raw_block_uranium', 
-        'thermal:raw_tin_block',
+        'immersiveengineering:raw_block_uranium', 'mekanism:block_raw_uranium',
+        'thermal:raw_tin_block', 'mekanism:block_raw_tin',
         
         //Ingots
         'create:brass_ingot', 
         'create:zinc_ingot', 
-        'eidolon:lead_ingot', 'immersiveengineering:ingot_lead', 'thermal:lead_ingot',
+        'eidolon:lead_ingot', 'immersiveengineering:ingot_lead', 'thermal:lead_ingot', 'mekanism:ingot_lead',
         'eidolon:silver_ingot', 'immersiveengineering:ingot_silver', 'darkerdepths:silver_ingot', 'iceandfire:silver_ingot', 'occultism:silver_ingot', 'thermal:silver_ingot',
         'immersiveengineering:ingot_aluminum', 
         'immersiveengineering:ingot_constantan', 'thermal:constantan_ingot',
         'immersiveengineering:ingot_electrum', 'createaddition:electrum_ingot', 'thermal:electrum_ingot',
         'immersiveengineering:ingot_nickel', 'thermal:nickel_ingot',
-        'immersiveengineering:ingot_steel',
-        'immersiveengineering:ingot_uranium', 
-        'thermal:bronze_ingot', 
+        'immersiveengineering:ingot_steel', 'ad_astra:steel_ingot', 'mekanism:ingot_steel',
+        'immersiveengineering:ingot_uranium', 'mekanism:ingot_uranium',
+        'mekanism:ingot_osmium',
+        'thermal:bronze_ingot', 'mekanism:ingot_bronze',
         'thermal:enderium_ingot',
         'thermal:invar_ingot', 
         'thermal:lumium_ingot', 
         'thermal:signalum_ingot', 
-        'thermal:tin_ingot',
+        'thermal:tin_ingot', 'mekanism:ingot_tin',
 
         //Plates
         'create:brass_sheet', 
@@ -92,11 +97,11 @@ JEIEvents.hideItems(event => {
         'immersiveengineering:plate_copper', 'create:copper_sheet', 'thermal:copper_plate',
         'immersiveengineering:plate_electrum', 'createaddition:electrum_sheet', 'thermal:electrum_plate',
         'immersiveengineering:plate_gold', 'create:golden_sheet', 'thermal:gold_plate',
-        'immersiveengineering:plate_iron', 'create:iron_sheet', 'thermal:iron_plate',
+        'immersiveengineering:plate_iron', 'create:iron_sheet', 'thermal:iron_plate', 'ad_astra:iron_plate',
         'immersiveengineering:plate_lead', 'thermal:lead_plate',
         'immersiveengineering:plate_nickel', 'thermal:nickel_plate',
         'immersiveengineering:plate_silver', 'thermal:silver_plate',
-        'immersiveengineering:plate_steel',
+        'immersiveengineering:plate_steel', 'ad_astra:steel_plate',
         'immersiveengineering:plate_uranium', 
         'thermal:bronze_plate',
         'thermal:enderium_plate',
@@ -138,43 +143,48 @@ JEIEvents.hideItems(event => {
         'immersiveengineering:nugget_constantan', 'thermal:constantan_nugget',
         'immersiveengineering:nugget_copper', 'create:copper_nugget', 'iceandfire:copper_nugget', 'thermal:copper_nugget', 'skilltree:copper_nugget',
         'immersiveengineering:nugget_electrum', 'createaddition:electrum_nugget', 'thermal:electrum_nugget',
-        'immersiveengineering:nugget_lead', 'eidolon:lead_nugget', 'thermal:lead_nugget',
+        'immersiveengineering:nugget_lead', 'eidolon:lead_nugget', 'thermal:lead_nugget', 'mekanism:nugget_lead',
         'immersiveengineering:nugget_nickel', 'thermal:nickel_nugget',
         'immersiveengineering:nugget_silver', 'eidolon:silver_nugget', 'iceandfire:silver_nugget', 'occultism:silver_nugget', 'thermal:silver_nugget',
-        'immersiveengineering:nugget_steel', 
-        'immersiveengineering:nugget_uranium', 
-        'thermal:bronze_nugget', 
+        'immersiveengineering:nugget_steel', 'ad_astra:steel_nugget', 'mekanism:nugget_steel',
+        'immersiveengineering:nugget_uranium', 'mekanism:nugget_uranium',
+        'mekanism:nugget_osmium',
+        'thermal:bronze_nugget', 'mekanism:nugget_bronze',
         'thermal:enderium_nugget', 
         'thermal:invar_nugget', 
         'thermal:lumium_nugget', 
         'thermal:netherite_nugget',
         'thermal:signalum_nugget', 
-        'thermal:tin_nugget',
+        'thermal:tin_nugget', 'mekanism:nugget_tin',
 
         //Dusts
         'ae2:ender_dust',
-        'createaddition:diamond_grit', 'thermal:diamond_dust',
+        'createaddition:diamond_grit', 'thermal:diamond_dust', 'mekanism:dust_diamond',
         'immersiveengineering:dust_aluminum', 
         'immersiveengineering:dust_constantan', 'thermal:constantan_dust',
-        'immersiveengineering:dust_copper', 'occultism:copper_dust', 'thermal:copper_dust',
+        'immersiveengineering:dust_copper', 'occultism:copper_dust', 'thermal:copper_dust', 'mekanism:dust_copper',
         'immersiveengineering:dust_electrum', 'thermal:electrum_dust',
-        'immersiveengineering:dust_gold', 'occultism:gold_dust', 'thermal:gold_dust',
-        'immersiveengineering:dust_iron', 'occultism:iron_dust', 'thermal:iron_dust',
-        'immersiveengineering:dust_lead', 'thermal:lead_dust',
+        'immersiveengineering:dust_gold', 'occultism:gold_dust', 'thermal:gold_dust', 'mekanism:dust_gold',
+        'immersiveengineering:dust_iron', 'occultism:iron_dust', 'thermal:iron_dust', 'mekanism:dust_iron',
+        'immersiveengineering:dust_lead', 'thermal:lead_dust', 'mekanism:dust_lead',
         'immersiveengineering:dust_nickel', 'thermal:nickel_dust',
         'immersiveengineering:dust_silver', 'occultism:silver_dust', 'thermal:silver_dust',
-        'immersiveengineering:dust_steel', 
-        'immersiveengineering:dust_sulfur', 'eidolon:sulfur',
-        'immersiveengineering:dust_uranium', 
-        'immersiveengineering:dust_wood',
-        'occultism:obsidian_dust',
-        'thermal:bronze_dust',
+        'immersiveengineering:dust_steel', 'mekanism:dust_steel',
+        'immersiveengineering:dust_sulfur', 'eidolon:sulfur', 'mekanism:dust_sulfur',
+        'immersiveengineering:dust_uranium', 'mekanism:dust_uranium',
+        'immersiveengineering:dust_wood', 'mekanism:sawdust',
+        'mekanism:dust_emerald',
+        'mekanism:dust_lapis_lazuli',
+        'mekanism:dust_osmium',
+        'mekanism:dust_quartz',
+        'occultism:obsidian_dust', 'mekanism:dust_obsidian',
+        'thermal:bronze_dust', 'mekanism:dust_bronze',
         'thermal:enderium_dust',
         'thermal:invar_dust',
         'thermal:lumium_dust',
-        'thermal:netherite_dust',
+        'thermal:netherite_dust', 'mekanism:dust_netherite',
         'thermal:signalum_dust',
-        'thermal:tin_dust',
+        'thermal:tin_dust', 'mekanism:dust_tin',
         
         //Blocks
         'create:brass_block', 
@@ -183,19 +193,20 @@ JEIEvents.hideItems(event => {
         'immersiveengineering:storage_aluminum', 
         'immersiveengineering:storage_constantan', 'thermal:constantan_block',
         'immersiveengineering:storage_electrum', 'thermal:electrum_block',
-        'immersiveengineering:storage_lead', 'eidolon:lead_block', 'thermal:lead_block',
+        'immersiveengineering:storage_lead', 'eidolon:lead_block', 'thermal:lead_block', 'mekanism:block_lead',
         'immersiveengineering:storage_nickel', 'thermal:nickel_block',
         'immersiveengineering:storage_silver', 'eidolon:silver_block', 'darkerdepths:silver_block', 'iceandfire:silver_block', 'occultism:silver_block', 'thermal:silver_block',
-        'immersiveengineering:storage_steel',
-        'immersiveengineering:storage_uranium', 
-        'thermal:bronze_block', 
+        'immersiveengineering:storage_steel', 'ad_astra:steel_block', 'mekanism:block_steel',
+        'immersiveengineering:storage_uranium', 'mekanism:block_uranium', 
+        'mekanism:block_osmium',
+        'thermal:bronze_block', 'mekanism:block_bronze', 
         'thermal:coal_coke_block',
         'thermal:enderium_block',
         'thermal:invar_block', 
         'thermal:lumium_block', 
         'thermal:signalum_block', 
-        'thermal:tin_block',
-        'quark:charcoal_block',
+        'thermal:tin_block', 'mekanism:block_tin',
+        'quark:charcoal_block', 'mekanism:block_charcoal',
 
         //Wires
         'createaddition:copper_wire',
@@ -203,7 +214,14 @@ JEIEvents.hideItems(event => {
         
         //Gems
         'iceandfire:sapphire_gem',
-        
+
+        //Dirty Materials
+        'alltheores:dirty_uranium_dust', 'alltheores:uranium_shard', 'alltheores:uranium_crystal', 'alltheores:uranium_clump',
+        'alltheores:dirty_tin_dust', 'alltheores:tin_shard', 'alltheores:tin_crystal', 'alltheores:tin_clump',
+        'alltheores:dirty_osmium_dust', 'alltheores:osmium_shard', 'alltheores:osmium_crystal', 'alltheores:osmium_clump',
+        'alltheores:dirty_lead_dust', 'alltheores:lead_shard', 'alltheores:lead_crystal', 'alltheores:lead_clump',
+        'alltheores:dirty_copper_dust', 'alltheores:copper_shard', 'alltheores:copper_crystal', 'alltheores:copper_clump',
+
         //Others
         'ae2:facade',
         'alltheores:other_aluminum_ore',
@@ -244,23 +262,30 @@ JEIEvents.hideItems(event => {
         'pneumaticcraft:oil_bucket',
         'pneumaticcraft:vegetable_oil_bucket',
         'thermal:crude_oil_bucket',
-        'thermal:creosote_bucket'
+        'thermal:creosote_bucket',
+        'ad_astra:oil_bucket',
+        'ad_astra:hammer',
+        'mysticalagradditions:nether_prosperity_ore',
+        'mysticalagradditions:end_prosperity_ore'
     ])
 })
 
 JEIEvents.hideFluids(event => {
-    event.hide('cofh_core:honey')
-    event.hide('createaddition:seed_oil')
-    event.hide('immersiveengineering:biodiesel')
-    event.hide('immersivepetroleum:diesel')
-    event.hide('immersivepetroleum:gasoline')
-    event.hide('immersivepetroleum:kerosene')
-    event.hide('immersivepetroleum:lubricant')
-    event.hide('pneumaticcraft:ethanol')
-    event.hide('pneumaticcraft:oil')
-    event.hide('pneumaticcraft:vegetable_oil')
-    event.hide('thermal:crude_oil')
-    event.hide('thermal:creosote')
+    event.hide([
+        'cofh_core:honey',
+        'createaddition:seed_oil',
+        'immersiveengineering:biodiesel',
+        'immersivepetroleum:diesel',
+        'immersivepetroleum:gasoline',
+        'immersivepetroleum:kerosene',
+        'immersivepetroleum:lubricant',
+        'pneumaticcraft:ethanol',
+        'pneumaticcraft:oil',
+        'pneumaticcraft:vegetable_oil',
+        'thermal:crude_oil',
+        'thermal:creosote',
+        'ad_astra:oil'
+    ])
 })
 
 JEIEvents.removeCategories(event => {

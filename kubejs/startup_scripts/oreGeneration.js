36cf6530b118c124c30368b4068b1ba33525ee61
SanoLink <link4francais@gmail.com>
2025-05-16 00:55:28 +0200

    [✨] Prosperity / Fluorite on Glacio

--- kubejs/startup_scripts/oreGeneration.js
+++ kubejs/startup_scripts/oreGeneration.js
@@ -1,9 +1,33 @@
-//Code By SanoLink For TechEV - Discovery
-//Last Modification : 1.1.0 
+/* 
+ _____                                             _____ 
+( ___ ) © SanoLink 2024/2025. All rights reserved.( ___ )
+ |   |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|   | 
+ |   |  _____         _     _____        ______    |   | 
+ |   | |_   _|__  ___| |__ | ____|_   __ \ \ \ \   |   | 
+ |   |   | |/ _ \/ __| '_ \|  _| \ \ / /  \ \ \ \  |   | 
+ |   |   | |  __/ (__| | | | |___ \ V /   / / / /  |   | 
+ |   |  _|_|\___|\___|_| |_|_____| \_/   /_/_/_/   |   | 
+ |   | |  _ \(_)___  ___ _____   _____ _ __ _   _  |   | 
+ |   | | | | | / __|/ __/ _ \ \ / / _ \ '__| | | | |   | 
+ |   | | |_| | \__ \ (_| (_) \ V /  __/ |  | |_| | |   | 
+ |   | |____/|_|___/\___\___/ \_/ \___|_|   \__, | |   | 
+ |   |                                      |___/  |   | 
+ |___|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|___| 
+(_____)         Last Modification : 1.4.0         (_____)
+
+*/
 
 WorldgenEvents.remove(event => {
 
     event.removeOres(ores => {
-        ores.blocks = ["eidolon:silver_ore", "eidolon:deep_silver_ore", "eidolon:lead_ore", "eidolon:deep_lead_ore"]
+        ores.blocks = [
+            "eidolon:silver_ore",
+            "eidolon:deep_silver_ore",
+            "eidolon:lead_ore",
+            "eidolon:deep_lead_ore",
+            "mysticalagradditions:nether_inferium_ore",
+            "mysticalagradditions:nether_prosperity_ore",
+            "mysticalagradditions:end_prosperity_ore"
+        ]
     })
 })

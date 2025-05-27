a860121b6e3d6854d613fc59c090b9087be0c20b
SanoLink <link4francais@gmail.com>
2025-05-27 16:50:26 +0200

    [✨] Added Missing ores on Laser Drill

---
+++ kubejs/server_scripts/constants/laserLens.js
@@ -0,0 +1,39 @@
+//priority: 11000
+/* 
+ _____                                             _____ 
+( ___ ) © SanoLink 2024/2025. All rights reserved.( ___ )
+ |   |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|   | 
+ |   |  _____         _     _____        ______    |   | 
+ |   | |_   _|__  ___| |__ | ____|_   __ \ \ \ \   |   | 
+ |   |   | |/ _ \/ __| '_ \|  _| \ \ / /  \ \ \ \  |   | 
+ |   |   | |  __/ (__| | | | |___ \ V /   / / / /  |   | 
+ |   |  _|_|\___|\___|_| |_|_____| \_/   /_/_/_/   |   | 
+ |   | |  _ \(_)___  ___ _____   _____ _ __ _   _  |   | 
+ |   | | | | | / __|/ __/ _ \ \ / / _ \ '__| | | | |   | 
+ |   | | |_| | \__ \ (_| (_) \ V /  __/ |  | |_| | |   | 
+ |   | |____/|_|___/\___\___/ \_/ \___|_|   \__, | |   | 
+ |   |                                      |___/  |   | 
+ |___|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|___| 
+(_____)         Last Modification : 1.3.6         (_____)
+
+*/
+
+const laserLens = {
+    white: 'industrialforegoing:laser_lens0',
+    orange: 'industrialforegoing:laser_lens1',
+    magenta: 'industrialforegoing:laser_lens2',
+    light_blue: 'industrialforegoing:laser_lens3',
+    yellow: 'industrialforegoing:laser_lens4',
+    lime: 'industrialforegoing:laser_lens5',
+    pink: 'industrialforegoing:laser_lens6',
+    gray: 'industrialforegoing:laser_lens7',
+    light_gray: 'industrialforegoing:laser_lens8',
+    cyan: 'industrialforegoing:laser_lens9',
+    purple: 'industrialforegoing:laser_lens10',
+    blue: 'industrialforegoing:laser_lens11',
+    brown: 'industrialforegoing:laser_lens12',
+    green: 'industrialforegoing:laser_lens13',
+    red: 'industrialforegoing:laser_lens14',
+    black: 'industrialforegoing:laser_lens15',
+
+}

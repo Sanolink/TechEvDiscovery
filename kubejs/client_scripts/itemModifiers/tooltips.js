ddf8c432ebac7ab89fc21ac90736c19541a6622f
SanoLink <link4francais@gmail.com>
2025-05-27 16:46:58 +0200

    [✨] Added a Tooltip for Lost Soul

--- kubejs/client_scripts/itemModifiers/tooltips.js
+++ kubejs/client_scripts/itemModifiers/tooltips.js
@@ -13,7 +13,7 @@
  |   | |____/|_|___/\___\___/ \_/ \___|_|   \__, | |   | 
  |   |                                      |___/  |   | 
  |___|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|___| 
-(_____)         Last Modification : 1.3.0         (_____)
+(_____)         Last Modification : 1.3.6         (_____)
 
 */
 
@@ -28,6 +28,9 @@ ItemEvents.tooltip(event => {
     //Blue Skies
     ['blue_skies:blue_journal','blue_skies:zeal_lighter','blue_skies:gatekeeper_spawn_egg'].forEach(item => {event.add(item, "§l§fThe §bGatekeeper §fis §CDISABLED")})
 
+    //Lost Soul
+    event.add('forbidden_arcanus:soul', "§l§fRight-click on a block to summon a §bLost Soul")
+    
     //Create Tooltips (Located in Create Lang)
     const ItemsCreateTooltips = [
         'create:chromatic_compound'

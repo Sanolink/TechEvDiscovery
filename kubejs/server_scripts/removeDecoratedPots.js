da9cc2d9c3208b9a2f02f5e2eff0853ef33e95c2
SanoLink <link4francais@gmail.com>
2025-06-20 02:35:51 +0200

    [✨⚙️] Removed all Decorated Pots Generation and Recipes temporarily

---
+++ kubejs/server_scripts/removeDecoratedPots.js
@@ -0,0 +1,30 @@
+/* 
+ _____                                             _____ 
+( ___ ) © SanoLink 2024/2025. All rights reserved.( ___ )
+ |   |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|   | 
+ |   |  _____         _     _____        ______    |   | 
+ |   | |_   _|__  ___| |__ | ____|_   __ \ \ \ \   |   | 
+ |   |   | |/ _ \/ __| '_ \|  _| \ \ / /  \ \ \ \  |   | 
+ |   |   | |  __/ (__| | | | |___ \ V /   / / / /  |   | 
+ |   |  _|_|\___|\___|_| |_|_____| \_/   /_/_/_/   |   | 
+ |   | |  _ \(_)___  ___ _____   _____ _ __ _   _  |   | 
+ |   | | | | | / __|/ __/ _ \ \ / / _ \ '__| | | | |   | 
+ |   | | |_| | \__ \ (_| (_) \ V /  __/ |  | |_| | |   | 
+ |   | |____/|_|___/\___\___/ \_/ \___|_|   \__, | |   | 
+ |   |                                      |___/  |   | 
+ |___|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|___| 
+(_____)         Last Modification : 1.3.8         (_____)
+
+*/
+
+// TO REMOVE When Cataclysm fixes the method of looting Decorated Pots
+MoreJSEvents.structureLoad(event => {
+    event.forEachPalettes(palette => {
+        palette.removeIf(blockData => {
+            if (blockData.id === "minecraft:decorated_pot") {
+                return true;
+            }
+            return false;
+        });
+    });
+});

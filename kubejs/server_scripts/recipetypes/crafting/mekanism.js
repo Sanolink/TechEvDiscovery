f224048bec4e4c10e83417e53cbb7a8b87847de0
SanoLink <link4francais@gmail.com>
2025-08-07 02:06:40 +0200

    [✨] Reworked almost all Mekanism recipes

---
+++ kubejs/server_scripts/recipetypes/crafting/mekanism.js
@@ -0,0 +1,43 @@
+/* 
+ _____                                             _____ 
+( ___ ) © SanoLink 2024/2025. All rights reserved.( ___ )
+ |   |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|   | 
+ |   |  _____         _     _____        ______    |   | 
+ |   | |_   _|__  ___| |__ | ____|_   __ \ \ \ \   |   | 
+ |   |   | |/ _ \/ __| '_ \|  _| \ \ / /  \ \ \ \  |   | 
+ |   |   | |  __/ (__| | | | |___ \ V /   / / / /  |   | 
+ |   |  _|_|\___|\___|_| |_|_____| \_/   /_/_/_/   |   | 
+ |   | |  _ \(_)___  ___ _____   _____ _ __ _   _  |   | 
+ |   | | | | | / __|/ __/ _ \ \ / / _ \ '__| | | | |   | 
+ |   | | |_| | \__ \ (_| (_) \ V /  __/ |  | |_| | |   | 
+ |   | |____/|_|___/\___\___/ \_/ \___|_|   \__, | |   | 
+ |   |                                      |___/  |   | 
+ |___|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|___| 
+(_____)         Last Modification : 1.4.0         (_____)
+
+*/
+
+ServerEvents.recipes(event => {
+
+    //Recipes
+    const recipes = [
+        //Goggle Unit
+        {
+            output: 'creategoggles:module_goggle_unit',
+            pattern: ["AGA", "AMA", "HHH"],
+            key: {
+                A: 'mekanism:alloy_infused',
+                G: 'create:goggles',
+                M: 'mekanism:module_base',
+                H: 'mekanism:hdpe_sheet'
+            }
+        }
+    ]
+
+    recipes.forEach(recipe => {
+        event.shaped(recipe.output, recipe.pattern, recipe.key).id(recipe.output)
+    })
+
+
+
+})

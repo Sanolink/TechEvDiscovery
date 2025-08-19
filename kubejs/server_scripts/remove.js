a2af7407a58e5ebb2b2a0aa9d52fae03555eb7e8
SanoLink <link4francais@gmail.com>
2025-08-19 02:53:46 +0200

    [✨] Reworked Create Tubes recipes with Shells and Precision Assembler

--- kubejs/server_scripts/remove.js
+++ kubejs/server_scripts/remove.js
@@ -13,7 +13,7 @@
  |   | |____/|_|___/\___\___/ \_/ \___|_|   \__, | |   | 
  |   |                                      |___/  |   | 
  |___|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|___| 
-(_____)         Last Modification : 1.4.3         (_____)
+(_____)         Last Modification : 1.4.4         (_____)
 
 */
 
@@ -125,6 +125,7 @@ ServerEvents.recipes(event => {
     event.remove({ id: 'create:crafting/materials/rose_quartz'})
     event.remove({ id: 'create:crafting/materials/andesite_alloy'})
     event.remove({ id: 'create:crafting/materials/andesite_alloy_from_zinc'})
+    event.remove({ id: 'create:crafting/materials/electron_tube'})
     event.remove({ id: 'create:mixing/andesite_alloy'})
     event.remove({ id: 'create:mixing/andesite_alloy_from_zinc'})
     event.remove({ id: 'create:pressing/desh_ingot'})

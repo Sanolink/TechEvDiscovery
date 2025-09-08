69885c5dc17fbcf6575eab314355a83c8214aee5
SanoLink <link4francais@gmail.com>
2025-09-09 01:12:46 +0200

    [🎨] Retextured FTBIC plates

--- kubejs/startup_scripts/modelsPredicates.js
+++ kubejs/startup_scripts/modelsPredicates.js
@@ -13,7 +13,7 @@
  |   | |____/|_|___/\___\___/ \_/ \___|_|   \__, | |   | 
  |   |                                      |___/  |   | 
  |___|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|___| 
-(_____)         Last Modification : 1.4.8         (_____)
+(_____)         Last Modification : 1.5.0         (_____)
 
 */
 
@@ -58,7 +58,8 @@ StartupEvents.postInit(event => {
         'ad_astra:ostrum_plate',
         'ad_astra:calorite_plate',
         'ad_astra:nephryx_plate',
-        'eidolon:arcane_gold_plate'
+        'eidolon:arcane_gold_plate',
+        "ftbic:carbon_plate"
     ]
     //All Plates
     Plates.forEach(plate => {

2591561a0382d4e479e5d846b9bf8d2ccff74509
SanoLink <link4francais@gmail.com>
2025-09-15 01:15:26 +0200

    [🛠️] Unified Regen Zinc in trades

--- kubejs/server_scripts/trade.js
+++ kubejs/server_scripts/trade.js
@@ -32,5 +32,9 @@ MoreJSEvents.playerStartTrading(event => {
             let count = o.getFirstInput().getCount()
             o.setFirstInput(Item.of("alltheores:silver_ingot", count))
         }
+        if (o.getSecondInput().getId() == "regen:zinc") {
+            let count = o.getSecondInput().getCount()
+            o.setSecondInput(Item.of("alltheores:zinc_ingot", count))
+        }
     })
 })

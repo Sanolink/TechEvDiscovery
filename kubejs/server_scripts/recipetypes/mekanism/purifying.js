3ece8cc9e3ae79935f6fab5393f6037006f8ce8c
SanoLink <link4francais@gmail.com>
2025-09-18 01:11:11 +0200

    [✨] Added Ad Astra materials slurry / clumps / crystals / shards / dirty dusts

--- kubejs/server_scripts/recipetypes/mekanism/purifying.js
+++ kubejs/server_scripts/recipetypes/mekanism/purifying.js
@@ -13,7 +13,7 @@
  |   | |____/|_|___/\___\___/ \_/ \___|_|   \__, | |   | 
  |   |                                      |___/  |   | 
  |___|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|___| 
-(_____)         Last Modification : 1.4.0         (_____)
+(_____)         Last Modification : 1.4.10        (_____)
 
 */
 
@@ -22,10 +22,36 @@ ServerEvents.recipes(event => {
     //Recipes 
     const recipes = []
 
+    const Clumps = ['desh', 'ostrum', 'calorite', 'nephryx'].forEach(mat => {
+        recipes.push(
+            {
+                id: `${mat}/clump/from_shard`,
+                input: MekaParseIngredient(`#mekanism:shards/${mat}`),
+                output: parseIngredient(`mekanism:clump_${mat}`)
+            },
+                        {
+                id: `${mat}/clump/from_ore`,
+                input: MekaParseIngredient(`#forge:ores/${mat}`),
+                output: ChanceOrCountItem(`mekanism:clump_${mat}`, 3)
+            },
+            {
+                id: `${mat}/clump/from_raw_ore`,
+                input: MekaParseIngredient(`#forge:raw_materials/${mat}`),
+                output: ChanceOrCountItem(`mekanism:clump_${mat}`, 2)
+            },
+            {
+                id: `${mat}/clump/from_raw_block`,
+                oxygen: 2,
+                input: MekaParseIngredient(`#forge:storage_blocks/raw_${mat}`),
+                output: ChanceOrCountItem(`mekanism:clump_${mat}`, 18)
+            }
+        )
+    })
+
     function productiveGene(attribute, input, output) {
         recipes.push({
             id: `gene_${attribute}_${input}_${output}`,
-            input: NBTItem('productivebees:gene', `{productivebees_gene_attribute:"${attribute}",productivebees_gene_purity:100,productivebees_gene_value:${input}}`),
+            input: { "ingredient": NBTItem('productivebees:gene', `{productivebees_gene_attribute:"${attribute}",productivebees_gene_purity:100,productivebees_gene_value:${input}}`)},
             output: NBTItem('productivebees:gene', `{productivebees_gene_attribute:"${attribute}",productivebees_gene_purity:100,productivebees_gene_value:${output}}`)
         })
     }
@@ -48,8 +74,8 @@ ServerEvents.recipes(event => {
     recipes.forEach(recipe => {
         let json = {
             type: 'mekanism:purifying',
-            chemicalInput: { "amount": recipe.oxygen || 1, "gas": "mekanism:oxygen" },
-            itemInput: { "ingredient": recipe.input },
+            chemicalInput: MekaGas("mekanism:oxygen", recipe.oxygen || 1),
+            itemInput: recipe.input,
             output: recipe.output
         }
         event.custom(json).id(`mekanism:purifying/${recipe.id}`)
